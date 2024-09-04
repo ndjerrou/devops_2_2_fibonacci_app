2c48a0a82af90311fd22ce8c7cdc0df5fac2a3a0
ndjerrou <n.djerrou@gmail.com>
2024-09-04 12:02:10 +0100

    working on API server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,37 @@
 const express = require('express');
+const redis = require('redis');
+
+const pg = require('pg');
+
+// init connection with pg through docker
+
+const redisClient = redis.createClient({
+  url: 'redis@redis',
+});
+
+const init = async () => {
+  await redisClient.connect();
+};
+
+redisClient.on('error', err => console.log('ERROR Connection Redis : ', err));
+
+init();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/values/current', async (req, res) => {
+  const values = await redisClient.hGetAll('values');
+
+  res.send(values);
+});
+
+// grabbing visited indexes
+app.get('/values/all', (req, res) => {
+  // with pg, grabd the visited indexes
+});
+
+app.post('/values', (req, res) => {
+  // combine redis and use the sub/pub patter to communicate with the worker
 });
 
 app.listen(5000, () => console.log('Listening on port 5000'));
