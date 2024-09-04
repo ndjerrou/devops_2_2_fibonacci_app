2d00497f348ec759af1cae0dafdd835d4a82559a
ndjerrou <n.djerrou@gmail.com>
2024-09-04 13:29:42 +0100

    established connection to redis and postgres

--- server/index.js
+++ server/index.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const redis = require('redis');
 
-const pg = require('pg');
+const { Pool } = require('pg');
 
 // init connection with pg through docker
+const pgClient = new Pool({
+  user: 'postgres',
+  host: 'postgres',
+  password: process.env.POSTGRES_PASSWORD,
+});
+
+pgClient.connect((err, client) => {
+  if (err) console.log('error connection', err);
+  client
+    .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+    .catch(err => console.error(err));
+
+  console.log('Connected to PostgreSQL instance');
+});
 
 const redisClient = redis.createClient({
   url: 'redis://redis',
+  // protocol//user:password@host:port
 });
 
 const init = async () => {
@@ -15,6 +30,7 @@ const init = async () => {
 
 redisClient.on('error', err => console.log('ERROR Connection Redis : ', err));
 redisClient.on('connect', () => console.log('Connected to Redis'));
+
 init();
 
 const app = express();
@@ -28,6 +44,7 @@ app.get('/values/current', async (req, res) => {
 // grabbing visited indexes
 app.get('/values/all', (req, res) => {
   // with pg, grabd the visited indexes
+  // @TODO
 });
 
 app.post('/values', (req, res) => {
